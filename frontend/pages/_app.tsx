61215b05bc7e8f344831629f9777fc87bdb754e9
dock108 <mikefusc@me.com>
2025-05-20 21:42:04 -0400

    feat(frontend): add daily mood check-in prompt

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { AuthProvider } from '../context/AuthContext'
 import { UserContentProvider } from '../context/UserContentContext'
+import DailyMoodPrompt from '../components/DailyMoodPrompt'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <UserContentProvider>
+        <DailyMoodPrompt />
         <Component {...pageProps} />
       </UserContentProvider>
     </AuthProvider>
